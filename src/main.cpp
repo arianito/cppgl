e2e42805622e331354b8d7c22b016434b737677c
ary4n <alikhaniaryan@live.com>
2017-05-29 04:37:25 +0430

    ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,135 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
 #include <iostream>
 
-#include <unistd.h>
-#include <ios>
-#include <fstream>
-#include <string>
+const GLchar* vertexSource = R"glsl(
+    #version 140
+    in vec2 position;
+    in vec3 color;
+    out vec3 Color;
+    void main()
+    {
+        Color = color;
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
 
-
-void fbsize_callback(GLFWwindow *window, int width, int height){
-    //printf("%ix%i\n", width, height);
-    glViewport(0, 0, width, height);
-}
-
+const GLchar* fragmentSource = R"glsl(
+    #version 140
+    in vec3 Color;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(Color, 1.0);
+    }
+)glsl";
 
 int main(){
     GLFWwindow *window;
     
-    if(!glfwInit()) {
-        
+    if(!glfwInit()) {   
         fprintf(stderr, "Failed to initialize GLFW\n");
         return -1;
     }
-    
-    glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
-    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // 4x antialiasing
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2); // We want OpenGL 3.3
-    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
+
+    glfwWindowHint(GLFW_SAMPLES, 4); 
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
     
     window = glfwCreateWindow(800, 600, "Aryan", nullptr, nullptr);
     
-    //glfwSetWindowPos(window, 100, 100);
-    // glfwSetWindowSizeCallback(window, fbsize_callback);
     
     if(window == nullptr) {
-        fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, "
-                "they are not 3.3 compatible. Try the 2.1 version of the "
-                "tutorials.\n");
+		fprintf(stderr, "Failed to load opengl.\n");
         glfwTerminate();
         return -1;
     }
-    
-    
-   glfwMakeContextCurrent(window);
-    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
-        fprintf(stderr, "Failed to initialize OpenGL context");
+
+    glfwSetWindowPos(window, 100, 100);
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize OpenGL context.\n");
         return -1;
     }
-    
-    // Ensure we can capture the escape key being pressed below
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+
+
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLfloat vertices[] = {
+        -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
+        -0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create an element array
+    GLuint ebo;
+    glGenBuffers(1, &ebo);
+
+    GLuint elements[] = {
+        0, 1, 2,
+        2, 3, 0
+    };
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), 0);
+
+    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+    glEnableVertexAttribArray(colAttrib);
+    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
+
     
-    // Dark blue background
-    glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
-    
-    do{
-        
-        // Clear the screen
+
+    do {
+         // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a rectangle from the 2 triangles using 6 indices
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         
-        // Swap buffers
         glfwSwapBuffers(window);
         glfwPollEvents();
         
-    }// Check if the ESC key was pressed or the window was closed
+    }
     while(glfwGetKey(window, GLFW_KEY_ESCAPE)!=GLFW_PRESS&&
           glfwWindowShouldClose(window)==0);
     
-        
-    // Close OpenGL window and terminate GLFW
     glfwTerminate();
     
     return 0;
